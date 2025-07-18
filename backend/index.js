ed2feb0fe69551a77289ec42a930a25e8e7393e0
Ujjwal Kumar <117573430+kumarujjwal1203@users.noreply.github.com>
2025-07-18 16:28:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.js';
 import taskRoutes from './routes/tasks.js';
+
+dotenv.config(); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
@@ -19,20 +21,16 @@ app.get("/", (req, res) => {
   res.send("Home Page - API is running");
 });
 
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
-const PORT = 5000;
-const MONGO_URI = 'mongodb://localhost:27017/todo'; 
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-    console.log("Connected to MongoDB");
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`erver running at http://localhost:${PORT}`);
+      console.log("Connected to MongoDB");
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
   });
-})
-.catch((err) => {
-  console.error(" MongoDB connection error:", err);
-});
